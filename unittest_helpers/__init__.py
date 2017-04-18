708b0147bbb223ca166b1d730e9e9b127407fe6f
Stanislav Z <stanislav@jirnexu.com>
2017-04-18 12:20:02 +0800

    Populated PEP8 test and added it to index

--- unittest_helpers/__init__.py
+++ unittest_helpers/__init__.py
@@ -1,1 +1,2 @@
 from test_case import TestCaseWithFixtures
+from test_pep8 import TestPEP
---
+++ unittest_helpers/test_pep8.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+
+import os
+import unittest
+import pep8
+
+ignore_patterns = ['.git', '.idea']
+
+
+class TestPEP(unittest.TestCase):
+    files = []
+
+    def test_pep8(self):
+        style = pep8.StyleGuide()
+        style.options.max_line_length = 120
+        python_files = []
+        for root, _, files in os.walk('./'):
+            if self.__ignore(root):
+                continue
+            python_files.extend([os.path.join(root, f) for f in files if f.endswith('.py')])
+        result = style.check_files(python_files)
+        self.assertEqual(result.total_errors, 0, 'PEP8 validation fails. Errors count: %s' % result.total_errors)
+
+    @staticmethod
+    def __ignore(directory):
+        for pattern in ignore_patterns:
+            if pattern in directory:
+                return True
+        return False
