759ec990bd4bd3157c534627a59ddfa436de7511
Stanislav Z <stanislav@jirnexu.com>
2017-04-21 17:47:35 +0800

    Added stubber

--- unittest_helpers/boto3_mock.py
+++ unittest_helpers/boto3_mock.py
@@ -1,11 +1,22 @@
 import boto3
 from botocore.stub import Stubber
 
-def client(*args, **kwargs):
-    client = boto3.client(*args, **kwargs)
-    stub = Stubber(client)
-    pass
+def get_client():
+    stubber = {'stubber': None}
 
-def resource(*args, **kwargs):
-    resource = boto3.resource(*args, **kwargs)
-    pass
+    def client(*args, **kwargs):
+        clt = boto3.client(*args, **kwargs)
+        stubber['stubber'] = Stubber(clt)
+        return clt
+
+    return stubber, client
+
+def get_resource():
+    stubber = {'stubber': None}
+
+    def resource(*args, **kwargs):
+        clt = boto3.resource(*args, **kwargs)
+        stubber['stubber'] = Stubber(clt)
+        return clt
+
+    return stubber, resource
